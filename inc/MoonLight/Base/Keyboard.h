7d49587561d1ad8c8348ff84e8e590c6c08d203c
dfranx <dfranx00@gmail.com>
2019-03-12 22:30:55 +0100

    Keyboard::KeyToString

--- inc/MoonLight/Base/Keyboard.h
+++ inc/MoonLight/Base/Keyboard.h
@@ -2,10 +2,17 @@
 #define __MOONLIGHT_KEYBOARD_H__
 
 #include <MoonLight/Base/Math.h>
+#include <string>
 
 namespace ml
 {
 	typedef ml::UInt8 VirtualKey;
+
+	class Keyboard
+	{
+	public:
+		static std::string KeyToString(VirtualKey vk);
+	};
 }
 
 #endif // __MOONLIGHT_KEYBOARD_H__
